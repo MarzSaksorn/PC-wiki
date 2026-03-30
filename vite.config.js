dc9d21697181029f5e67a0c7e79a418781121716
Marzsaksorn <mark94849@proton.me>
2026-03-31 06:39:13 +0700

    Revert "Preview port to 5173"
    This reverts commit f4f5b609aa746aab0c2491d506fb4dc0dde9b1fb.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  preview: {
-    port: 5173,
-  },
   plugins: [
     react(),
     tailwindcss(),
   ],
-});
+})
